7490bf4690aa7172b23ab9625e8c7e320824ac94
gwonsheep <jae-kwen@daum.net>
2024-11-11 23:32:06 +0900

    Initial Commit (2024.11.11.)

--- src/main/java/com/jproject/wee/controller/WeeController.java
+++ src/main/java/com/jproject/wee/controller/WeeController.java
@@ -2,20 +2,20 @@ package com.jproject.wee.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/wee")
 public class WeeController {
-
     @GetMapping("/test")
-    public String test() {
-
-        return "/weeMain/testPage";
-
+    public ResponseEntity<String> test() {
+        return ResponseEntity.status(HttpStatus.OK).body("test");
     }
-
 }
